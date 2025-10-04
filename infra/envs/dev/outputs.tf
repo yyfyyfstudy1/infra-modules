f064eb22a39ce774ba4e9f0de678c8e8e9857ed2
yyfyyfstudy1 <yifan.yu@uqconnect.edu.au>
2025-10-05 03:11:25 +1100

    test: comment out dev test bucket for GitOps workflow testing
    - Temporarily comment out test_bucket module in dev environment
    - Comment out corresponding outputs for test_bucket
    - This change will be used to test the GitOps workflow:
      - Jenkins should auto-trigger on PR creation
      - terraform plan should show bucket deletion
      - PR should receive automatic comment with plan results
      - After PR merge, terraform apply should execute automatically

--- infra/envs/dev/outputs.tf
+++ infra/envs/dev/outputs.tf
@@ -13,15 +13,16 @@ output "lambda_function_arn" {
   value       = module.lambda_join.function_arn
 }
 
-output "test_bucket_name" {
-  description = "测试 S3 桶名称"
-  value       = module.test_bucket.bucket_id
-}
-
-output "test_bucket_arn" {
-  description = "测试 S3 桶 ARN"
-  value       = module.test_bucket.bucket_arn
-}
+# 测试用的 S3 桶输出 - 临时注释用于测试 GitOps 工作流
+# output "test_bucket_name" {
+#   description = "测试 S3 桶名称"
+#   value       = module.test_bucket.bucket_id
+# }
+#
+# output "test_bucket_arn" {
+#   description = "测试 S3 桶 ARN"
+#   value       = module.test_bucket.bucket_arn
+# }
 
 output "test_cicd_bucket_name" {
   description = "CI/CD 测试 S3 桶名称"
