f8bf35a69fe56b5094a51de21251e89866506a86
MengtingMT <114296914+MengtingMT@users.noreply.github.com>
2025-10-05 19:26:46 +1100

    Update main.tf

--- infra/app/main.tf
+++ infra/app/main.tf
@@ -85,7 +85,7 @@ module "test_bucket" {
 
   source = "../modules/s3_bucket"
 
-  bucket_name        = "${local.name_prefix}-test-bucket-1"
+  bucket_name        = "${local.name_prefix}-test-bucket-mt"
   versioning_enabled = false
   force_destroy      = true
   tags               = local.common_tags
